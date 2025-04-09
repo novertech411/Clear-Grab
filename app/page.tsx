f61667078f57f63e088c48f19828f04850608403
novertech411 <novertech4@gmail.com>
2025-04-09 07:52:16 +0100

    working on the role call

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,5 @@ import Footer from "@/src/components/ui/Footer";
 import Navbar from "@/src/components/ui/Navbar";
 
 export default function Home() {
-  return (
-    <div className=" bg-white h-screen">
-      <Navbar />
-      <Footer />
-    </div>
-  );
+  return <div className=" bg-white h-screen"></div>;
 }
--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -4,15 +4,20 @@ import React from "react";
 import logo from "@/public/images/Logo Icon- main.svg";
 export default function Navbar() {
   return (
-    <div className=" text-black containers flex justify-between  border-b border-gray-200 py-5 items-center">
-      <div className="w-6">
-        <Image src={logo} alt=" " />
-      </div>
+    <div className="  bg-white">
+      <div className=" containers  text-black flex justify-between  border-b border-gray-200 py-5 items-center ">
+        <div className="w-6">
+          <Image src={logo} alt=" " />
+        </div>
 
-      <div className="flex items-center gap-7 ">
-        <div className=" bg-green-600  py-2 px-4 rounded-xl"> Sell</div>
-        <div>
-          <FaUserCircle className="w-7 h-7 text-gray-400" />
+        <div className="flex items-center gap-7 ">
+          <div className=" bg-green-600 text-white  py-2 px-4 rounded-xl">
+            {" "}
+            Sell
+          </div>
+          <div>
+            <FaUserCircle className="w-7 h-7 text-gray-400" />
+          </div>
         </div>
       </div>
     </div>
